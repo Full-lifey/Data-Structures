ffbdf6f314a264cb4272ee8f901822021aa3b6e7
Joel Perez <joelness03@gmail.com>
2019-10-15 14:13:43 -0600

    LRU init done, WIP LRU set

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,8 @@
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('../doubly_linked_list')
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +11,12 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.size = 0
+        self.limit = limit
+        self.lookup = {}
+        self.storage = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +25,12 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+        if self.lookup[key]:
+            pass
+        else:
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +42,15 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+        if self.lookup[key]:
+            self.lookup[key] = value
+            current = self.storage.head
+            while current:
+                if current.value[key]:
+                    current.value[key] = value
+                current = current.next
+        self.storage.add_to_head({key: value})
+        if self.size > self.limit:
+            self.storage.remove_from_tail()
--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -12,12 +12,14 @@ class Queue:
 
     def enqueue(self, value):
         self.storage.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        if self.len() == 0:
+        if self.size == 0:
             return None
         else:
+            self.size -= 1
             return self.storage.remove_from_head()
 
     def len(self):
-        return len(self.storage)
+        return self.size
